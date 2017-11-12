2f23393ddfcd1ea47b7cb9ee354793b6475a4461
chares strube <charlesstrube@gmail.com>
2017-11-12 17:53:28 +0100

    documention vew

--- controllers/controller.view.php
+++ controllers/controller.view.php
@@ -1,5 +1,10 @@
 <?php
-
+/**
+ * get view
+ * @param  string $path  get view path with keyword
+ * @param  array  $views all the info
+ * @return void
+ */
 function view($path, $views) {
     require_once VIEWS_ROOT . '/view.header.php';
     require_once VIEWS_ROOT . '/view.' . $path . '.php';
