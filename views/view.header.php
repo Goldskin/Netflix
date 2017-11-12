2b41053fd498d12c1558a5c4d1ea9acd1a4c4f3c
chares strube <charlesstrube@gmail.com>
2017-11-12 21:44:50 +0100

    add color application and img link

--- views/view.header.php
+++ views/view.header.php
@@ -1,4 +1,6 @@
-<!DOCTYPE html>
+<?php
+$uri_parts = explode('?', $_SERVER['REQUEST_URI'], 2);
+?><!DOCTYPE html>
 <html>
     <head>
         <meta charset="utf-8">
@@ -7,10 +9,11 @@
         <link rel="stylesheet" href="https:cdn.jsdelivr.net/foundation/6.2.4/foundation.min.css">
         <link href="https://fonts.googleapis.com/css?family=Roboto:400,700" rel="stylesheet">
         <link rel="stylesheet" href="dist/css/style.min.css">
+        <meta name="theme-color" content="#141414" />
     </head>
     <body class="container">
         <div class="row">
             <div class="columns">
-                <img src="dist/img/netflix-logo.svg" alt="Netflix">
+                <a href="<?= '//' . $_SERVER['HTTP_HOST'] . $uri_parts[0] ?>"><img src="dist/img/netflix-logo.svg" alt="Netflix"></a>
             </div>
             <div class="columns">
