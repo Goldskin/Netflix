2013ddb884bd659ed8f28aee3a5befa5e49f4fb4
chares strube <charlesstrube@gmail.com>
2017-11-12 18:50:16 +0100

    Update controller service

--- controllers/controller.service.php
+++ controllers/controller.service.php
@@ -1,15 +1,6 @@
 <?php
 require_once CLASSES_ROOT . '/class.main.php';
 
-function rounder ($difference)
-{
-    if ($difference > 0) {
-        $difference = ceil( ($difference) * 100) / 100;
-    } else {
-        $difference = floor( ($difference) * 100) / 100;
-    }
-    return $difference;
-}
 
 
 /**
@@ -34,68 +25,18 @@ function controller (Service $Service) {
         // get current price of service
         $currentTarif = $Service->getActiveTarif($dateCurrent);
 
-        // price repartition by month
-        $totalUser = $Service->getActiveUsersNumber($dateCurrent);
+        // get active user
+        $Users      = $Service->getActiveUsers($dateCurrent);
+        $totalUser  = count($Users);
 
-        // Calc right price for current month
-        // ----------
-        // get approximative price and apply to each one
-        $repartitionAprox = rounder($currentTarif->get() / $totalUser);
-
-        // add for each users the approximative price
-        $userRepartition = [];
-        for ($i = 0; $i < $totalUser; $i++) {
-            $userRepartition[$i] = rounder($repartitionAprox);
-        }
-
-        // get the difference between real price and approximative price
-        $difference = $currentTarif->get() - array_sum($userRepartition);
-        $difference = round($difference * 100) / 100;
-
-
-        // correct the the approximative price
-        if ($difference < 0) {
-            for ($i = $totalUser - 1; $i >= 0; $i--) {
-                // get the price difference for the current user
-                $numberNeeded = rounder($difference / $totalUser);
-
-                // subtrack to the difference
-                $difference -= $numberNeeded;
-
-                // add or subtrack the difference
-                $userRepartition[$i] += $numberNeeded;
-            }
-        } else {
-            for ($i = 0; $i < $totalUser; $i++) {
-                // get the price difference for the current user
-                $numberNeeded = rounder($difference / $totalUser);
-
-                // subtrack to the difference
-                $difference -= $numberNeeded;
-
-                // add or subtrack the difference
-                $userRepartition[$i] += $numberNeeded;
-            }
-        }
-
-        ksort($userRepartition);
-
+        // get split bill
+        $userRepartition = $currentTarif->split($totalUser);
 
         // Applying bill
-        $i = 0;
-        Main::each($Service->user(), function ($User) use ($userRepartition, $dateCurrent, &$i)
-        {
-            Main::each($User->interval(), function ($Interval) use ($userRepartition, $dateCurrent, &$User, &$i)
-            {
-
-                if ($Interval->between($dateCurrent)) {
-                    $date = clone $dateCurrent;
-                    $User->bill( (new Price ())->set($userRepartition[$i])->date($date) );
-                    $i++;
-                }
-            });
-        });
-
+        foreach ($Users as $key => $User) {
+            $date = clone $dateCurrent;
+            $User->bill( (new Price ())->set($userRepartition[$key])->date($date) );
+        }
 
     }
 
