a29e5efb3ee7633c06025680a4fce17310a56970
chares strube <charlesstrube@gmail.com>
2017-11-18 22:30:49 +0100

    Modification view home total

--- controllers/home.controller.php
+++ controllers/home.controller.php
@@ -13,9 +13,10 @@ class HomeController extends Controller
         $Netflix = $Model->get();
 
         $views = [];
-        $views['total']['payed'] = 0;
-        $views['total']['unpayed'] = 0;
-        $views['total']['advance'] = 0;
+        $views['resume']  = [];
+        $views['payed']   = new Price ();
+        $views['unpayed'] = new Price ();
+        $views['advance'] = new Price ();
 
         $Netflix::each($Netflix->user(), function ($User) use (&$views)
         {
@@ -25,18 +26,20 @@ class HomeController extends Controller
                      'id' => is_null($User->id()) ? '' : $User->id(),
                      'payed' => is_null($User->payed()) ? '' : $User->payed()->format(),
                      'unpayed' => is_null($User->unpayed()) ? '' : $User->unpayed()->format(),
-                     'advance' => is_null($User->advance()) ? '' : $User->advance()->format()
+                     'advance' => is_null($User->advance()) ? '' : $User->advance()->format(),
+                     'url' => URL . '/user/' . (is_null($User->id()) ? '' : $User->id())
                 ];
-                $views['total']['payed'] += is_null($User->payed()) ? 0 : $User->payed()->get();
-                $views['total']['unpayed'] += is_null($User->unpayed()) ? 0 : $User->unpayed()->get();
-                $views['total']['advance'] += is_null($User->advance()) ? 0 : $User->advance()->get();
+                is_null($User->payed())   ? 0 : $views['payed']  ->set($User->payed());
+                is_null($User->unpayed()) ? 0 : $views['unpayed']->set($User->unpayed());
+                is_null($User->advance()) ? 0 : $views['advance']->set($User->advance());
             }
         });
-
-        $views['total']['payed'] = $views['total']['payed'] == 0 ? '' : $views['total']['payed'] . ' &euro;';
-        $views['total']['unpayed'] = $views['total']['unpayed'] == 0 ? '' : $views['total']['unpayed'] . ' &euro;';
-        $views['total']['advance'] = $views['total']['advance'] == 0 ? '' : $views['total']['advance'] . ' &euro;';
+        $views['payed']   = $views['payed']  ->get() == 0 ? '' : $views['payed']  ->format();
+        $views['unpayed'] = $views['unpayed']->get() == 0 ? '' : $views['unpayed']->format();
+        $views['advance'] = $views['advance']->get() == 0 ? '' : $views['advance']->format();
 
         $this->set($views)->render('index');
+
+        // echo "<pre>", var_dump($views), "</pre>";
     }
 }
--- views/home/index.php
+++ views/home/index.php
@@ -5,7 +5,7 @@
             <th>Payé</th>
             <th>Restant à payer</th>
             <th>Avance</th>
-            <th>Détails</th>
+            <th class="text-right">Détails</th>
         </tr>
     </thead>
     <tbody>
@@ -15,18 +15,17 @@
                 <td><?= $view['payed'] ?></td>
                 <td><?= $view['unpayed'] ?></td>
                 <td><?= $view['advance']  ?></td>
-                <td><a class="button alert no-marg" href="<?= URL . '/user/' . strtolower($view['id']) ?>">Détails</td>
+                <td class="text-right"><a class="button alert no-marg text-right" href="<?= $view['url']  ?>">Détails</td>
             </tr>
         <?php endforeach; ?>
     </tbody>
     <tfoot>
         <tr>
             <td>Total</td>
-            <td><?= $total['payed'] ?></td>
-            <td><?= $total['unpayed'] ?></td>
-            <td><?= $total['advance'] ?></td>
+            <td><?= $payed ?></td>
+            <td><?= $unpayed ?></td>
+            <td><?= $advance ?></td>
             <td>&nbsp;</td>
-
         </tr>
     </tfoot>
 </table>
