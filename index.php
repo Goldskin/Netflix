f4bb44636f32750b3a8bfd9041e6f075c0c82a95
chares strube <charlesstrube@gmail.com>
2018-06-07 22:14:41 +0200

    Wip, proper router

--- index.php
+++ index.php
@@ -2,22 +2,53 @@
 define('WEBROOT', str_replace('index.php', '', $_SERVER['SCRIPT_NAME']));
 define('ROOT',    str_replace('index.php', '', $_SERVER['SCRIPT_FILENAME']));
 define('URL',     '//'. (($_SERVER['SERVER_NAME'] === 'localhost') ? 'localhost/charles' : $_SERVER['SERVER_NAME']));
+define('CORRECT_PATH',     ($_SERVER['SERVER_NAME'] === 'localhost') ? '/charles' : '');
 
 define('CLASSES_ROOT',     ROOT . 'classes/');
 define('VIEWS_ROOT',       ROOT . 'views/');
+define('VENDOR_ROOT',      ROOT . 'vendor/');
 define('CONTROLLERS_ROOT', ROOT . 'controllers/');
 define('MODELS_ROOT',      ROOT . 'models/');
 define('DATAS_ROOT',       ROOT . 'datas/');
 define('ROUTE',            ROOT . 'route/');
 
-require_once ROUTE . 'core.controller.php';
-require_once ROUTE . 'collection.controller.php';
+require VENDOR_ROOT . 'autoload.php';
 
-$RouteCollection = new RouteCollection($_GET['p']);
+$dispatcher = FastRoute\simpleDispatcher(function(FastRoute\RouteCollector $r) {
+    $r->get(CORRECT_PATH . '/all.json', 'get_all_users_handler');
+    // {id} must be a number (\d+)
+    $r->get('GET', CORRECT_PATH . '/id/{id:\d+}', 'get_user_handler');
+    // The /{title} suffix is optional
+    $r->addRoute('GET', CORRECT_PATH . '/articles/{id:\d+}[/{title}]', 'get_article_handler');
+});
 
-$RouteCollection
-    ->add((new Route ('user'))->param(1)->controller('user')->action('index'))
-    ->add((new Route ('/'))->controller('home')->action('index'))
-    ->add((new Route ('bill'))->param(1)->controller('bill')->action('index'));
+// Fetch method and URI from somewhere
+$httpMethod = $_SERVER['REQUEST_METHOD'];
+$uri = $_SERVER['REQUEST_URI'];
 
-$RouteCollection->redirect();
+var_dump(CORRECT_PATH, $httpMethod, $uri);
+
+// Strip query string (?foo=bar) and decode URI
+if (false !== $pos = strpos($uri, '?')) {
+    $uri = substr($uri, 0, $pos);
+}
+$uri = rawurldecode($uri);
+
+$routeInfo = $dispatcher->dispatch($httpMethod, $uri);
+switch ($routeInfo[0]) {
+    case FastRoute\Dispatcher::NOT_FOUND:
+        echo 'not found';
+        break;
+    case FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
+        $allowedMethods = $routeInfo[1];
+        var_dump($allowedMethods);
+        // ... 405 Method Not Allowed
+        break;
+    case FastRoute\Dispatcher::FOUND:
+        $handler = $routeInfo[1];
+        $vars = $routeInfo[2];
+
+        var_dump($handler, $vars);
+        // ... call $handler with $vars
+        break;
+}
