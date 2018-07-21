533a28719f83aa452c0140b9cd335abba36d7c21
chares strube <charlesstrube@gmail.com>
2018-07-21 19:11:39 +0200

    Added new header to support cros origin

--- controllers/all.php
+++ controllers/all.php
@@ -3,5 +3,6 @@
 require_once MODELS_ROOT . 'service.model.php';
 
 $ServiceModel = (new serviceModel ())->getModel();
+header("Access-Control-Allow-Origin: *");
 header('Content-Type: application/json');
 echo json_encode(Main::$all);
--- controllers/id.php
+++ controllers/id.php
@@ -9,6 +9,6 @@ $test = preg_match_all($regex, $vars['id'], $match);
 
 
 $item = $ServiceModel->getMultipleIds($match[1]);
-
+header("Access-Control-Allow-Origin: *");
 header('Content-Type: application/json');
 echo json_encode($item);
